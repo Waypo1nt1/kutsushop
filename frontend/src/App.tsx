21d97e22d388b9d731d4d536b7819f426a839ffc
Waypo1nt1 <popovsasa997@gmail.com>
2023-12-18 22:29:35 +0300

    added tailwind, daisyui, frontend pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import {Footer} from './components/Footer'
+import {AdminPanel} from './components/AdminPanel'
+import {LoginForm} from './components/LoginForm'
+
+import './app.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
+  let isLoggedIn = true
+  let content;
+  if (isLoggedIn) {
+    content = <AdminPanel />
+  } else {
+    content = <LoginForm />
+  }
   return (
     <>
-      <div>
+      {content}
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
+
+/*
+<div className="px-5 pt-10 w-full h-full flex flex-col items-center">
+      <h1>
+        </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="btn" onClick={() => setCount(count+1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+</div>
+        */

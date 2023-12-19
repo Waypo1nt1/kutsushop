c8ddbd0f3e643ad16f3c52a09cdb88848ecb5f96
Waypo1nt1 <popovsasa997@gmail.com>
2023-12-20 01:14:30 +0300

    kostbli

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import { useEffect } from 'react'
 import {Footer} from './components/Footer'
 import {AdminPanel} from './components/AdminPanel'
 import {LoginForm} from './components/LoginForm'
-import axios from "axios"
 
 import './index.css'
 
-export type Shoes = {
-  id: number
-  shoes_name: string
-}
+
 
 
 function App() {
-  const [shoes, setShoes] = useState<Shoes[] | null>()
   const [isLoggedIn, setLoggedIn] = useState(true)
-
-  useEffect(() => {
-    const url = "http://localhost:3000/data"
-    axios.get(url).then((response) => {
-      setShoes(response.data)
-    })
-  }, [])
-
-
 
   return (
     <>
-    {isLoggedIn ? <AdminPanel shoes={shoes ?? []} /> : <LoginForm />}
+    {isLoggedIn ? <AdminPanel /> : <LoginForm />}
       <Footer />
     </>
   )

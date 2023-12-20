cdfb7d360d9ab0577c3f4400fd7032d137ea5498
Waypo1nt1 <popovsasa997@gmail.com>
2023-12-20 20:44:39 +0300

    fixes

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -1,13 +1,13 @@
 export function ProductForm() {
     return (
-        <div className="bg-gray-100 dark:bg-gray-800 transition-colors duration-300">
+        <div className="bg-gray-100 dark:bg-gray-800 transition-colors duration-300 min-h-full">
   <div className="container mx-auto p-4">
     <div className="bg-white dark:bg-gray-700 shadow rounded-lg p-6">
       <h1 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
-        Personal Information
+        Добавление заказа
       </h1>
       <p className="text-gray-600 dark:text-gray-300 mb-6">
-        Use a permanent address where you can receive mail.
+        Укажите информацию о заказе
       </p>
       <form>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">

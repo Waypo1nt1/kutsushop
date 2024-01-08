739db4cf2546e34dc17bc9ca2a0008c488c8b8de
Waypo1nt1 <popovsasa997@gmail.com>
2024-01-08 21:38:22 +0300

    added more columns in /viewdata/shoes

--- frontend/src/components/pages/shoes.tsx
+++ frontend/src/components/pages/shoes.tsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 interface Shoes {
   id: number
   shoes_name: string
+  shoes_color: string
+  shoes_size: number
 }
 
 const Orders = () => {
@@ -20,14 +22,18 @@ const Orders = () => {
   return (
     <PageWrapper header="Модели обуви">
       <div className={`${shoes.length ? 'opacity-100 viisble' : 'opacity-0 invisible'} transition-opacity duration-500`}>
-        <article className="grid grid-cols-10  px-10 py-5 text-white">
+        <article className="grid grid-cols-4 px-10 py-5 text-white text-center">
           <div className="underline text-xl">Id</div>
-          <div className="underline text-xl col-span-9">Модель обуви</div>
+          <div className="underline text-xl">Модель обуви</div>
+          <div className="underline text-xl">Цвет обуви</div>
+          <div className="underline text-xl">Размер обуви</div>
         </article>
         {shoes.map(shoe => (
-          <article key={shoe.id} className={`grid grid-cols-10 bg-[#333333] px-10 py-5 border mb-3`}>
+          <article key={shoe.id} className={`grid grid-cols-4 bg-[#333333] px-10 py-5 border mb-3 text-center`}>
             <div>{shoe.id}</div>
-            <div className="col-span-9">{shoe.shoes_name}</div>
+            <div>{shoe.shoes_name}</div>
+            <div>{shoe.shoes_color}</div>
+            <div>{shoe.shoes_size}</div>
           </article>
         ))}
       </div>
